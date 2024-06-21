7adb10bfeafc620acb98a31580364c6f2c56a1f9
AidanV <aidanvanduyne@gmail.com>
2024-06-21 16:04:08 -0700

    working one line of input

--- app/Main.hs
+++ app/Main.hs
@@ -8,132 +8,78 @@ import Lens.Micro.TH
 import Lens.Micro.Mtl
 import qualified Graphics.Vty as V
 
-import qualified Data.Text as T
-
-import Brick.Forms
-  ( Form
-  , newForm
-  , formState
-  , formFocus
-  , setFieldValid
-  , renderForm
-  , handleFormEvent
-  , invalidFields
-  , allFieldsValid
-  , focusedFormInputAttr
-  , invalidFormInputAttr
-  , checkboxField
-  , radioField
-  , editShowableField
-  , editTextField
-  , editPasswordField
-  , (@@=)
-  )
-
 import qualified Brick.Main as M
+import qualified Brick.Types as T
 import Brick.Widgets.Core
   ( (<+>)
   , (<=>)
   , hLimit
   , vLimit
-  , withBorderStyle
   , str
-  , txt
   )
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Widgets.Edit as E
-import qualified Brick.Widgets.Border as B
-import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.AttrMap as A
 import qualified Brick.Focus as F
 import Brick.Util (on)
-import Brick.Types (Widget, BrickEvent(VtyEvent))
 
-data Name = EquationField deriving (Eq, Ord, Show)
+data Name = EditEquation deriving (Ord, Show, Eq)
 
-data FormInfo = FormInfo { _equation :: T.Text } deriving (Show)
+data St =
+    St { _focusRing :: F.FocusRing Name
+       , _edit1 :: E.Editor String Name
+       }
 
-makeLenses ''FormInfo
+makeLenses ''St
 
-mkForm :: FormInfo -> Form FormInfo e Name
-mkForm = 
-    newForm [ editTextField equation EquationField (Just 1) ]
---drawUI :: [T.Widget String]
---drawUI = [ui]
---    where
---        e1 = (E.renderEditor (str . unlines)) "edit 1"
---        -- e2 = F.withFocusRing (st^.focusRing) (E.renderEditor (str . unlines)) (st^.edit2)
---
---        ui = C.center $
---            (str "Input 1 (unlimited): " <+> (hLimit 30 $ e1 "test")) <=>
---            str " " <=>
---            str "Esc to quit."
+drawUI :: St -> [T.Widget Name]
+drawUI st = [ui]
+    where
+        e1 = F.withFocusRing (st^.focusRing) (E.renderEditor (str . unlines)) (st^.edit1)
 
-draw :: Form FormInfo e Name -> Widget ()
-draw f =
-    withBorderStyle BS.unicodeRounded $
-    B.borderWithLabel (str "label") $
-    vLimit 5 $
-    C.vCenter $
-    renderForm f
-    --(str "Name: " <+>) @@=
-      --(mkForm (FormInfo { _equation = "" }))
+        ui = C.center $
+            (str "Input 1 (unlimited): " <+> hLimit 30 e1) <=>
+            str " " <=>
+            str "Press Tab to switch between editors, Esc to quit."
 
---appEvent :: T.BrickEvent String e -> T.EventM String () ()
---appEvent (T.VtyEvent (V.EvKey V.KEsc [])) =
---    M.halt
-----appEvent (T.VtyEvent (V.EvKey (V.KChar '\t') [])) =
-----    focusRing %= F.focusNext
-----appEvent (T.VtyEvent (V.EvKey V.KBackTab [])) =
-----    focusRing %= F.focusPrev
---appEvent ev =   
---    E.handleEditorEvent ev 
---      Just Edit2 -> zoom edit2 $ E.handleEditorEvent ev
---      Nothing -> return ()
+appEvent :: T.BrickEvent Name e -> T.EventM Name St ()
+appEvent (T.VtyEvent (V.EvKey V.KEsc [])) =
+    M.halt
+appEvent (T.VtyEvent (V.EvKey (V.KChar '\t') [])) =
+    focusRing %= F.focusNext
+appEvent (T.VtyEvent (V.EvKey V.KBackTab [])) =
+    focusRing %= F.focusPrev
+appEvent ev = do
+    r <- use focusRing
+    case F.focusGetCurrent r of
+      Just EditEquation -> zoom edit1 $ E.handleEditorEvent ev
+      Nothing -> return ()
 
+initialState :: St
+initialState =
+    St (F.focusRing [EditEquation])
+       (E.editor EditEquation (Just 1) "")
 
---theMap :: A.AttrMap
---theMap = A.attrMap V.defAttr
---    [ (E.editAttr,                   V.white `on` V.blue)
---    , (E.editFocusedAttr,            V.black `on` V.white)
---    ]
+theMap :: A.AttrMap
+theMap = A.attrMap V.defAttr
+    [ (E.editAttr,                   V.white `on` V.blue)
+    , (E.editFocusedAttr,            V.black `on` V.yellow)
+    ]
 
---appCursor :: St -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
---appCursor = F.focusRingCursor (^.focusRing)
+appCursor :: St -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
+appCursor = F.focusRingCursor (^.focusRing)
 
---theApp :: M.App e
---theApp =
---    M.App { M.appDraw = drawUI
---          --, M.appChooseCursor = appCursor
---          , M.appHandleEvent = appEvent
---          , M.appStartEvent = return ()
---          , M.appAttrMap = const theMap
---          }
+theApp :: M.App St e Name
+theApp =
+    M.App { M.appDraw = drawUI
+          , M.appChooseCursor = appCursor
+          , M.appHandleEvent = appEvent
+          , M.appStartEvent = return ()
+          , M.appAttrMap = const theMap
+          }
 
 main :: IO ()
-main = 
-    let initialState = FormInfo { _equation = "" }
-        f = mkForm initialState
-        app = M.App { M.appDraw = draw f
-                    , M.appHandleEvent = \ev -> do
-                      case ev of
-                          VtyEvent (V.EvResize {}) -> return ()
-                          VtyEvent (V.EvKey V.KEsc []) -> M.halt
-                          -- Enter quits only when we aren't in the multi-line editor.
-                          _ -> do
-                              handleFormEvent ev
-
-                              -- Example of external validation:
-                              -- Require age field to contain a value that is at least 18.
-
-                    , M.appStartEvent = return ()
-                    }
-     in
-      M.defaultMain app initialState
-    --putStrLn "yuh huh"
---main = do
---    st <- M.defaultMain theApp
---    putStrLn "In input 1 you entered:\n"
---    putStrLn $ unlines $ E.getEditContents "edit 1" 
---    putStrLn "In input 2 you entered:\n"
---    putStrLn $ unlines $ E.getEditContents $ st^.edit2
+main = do
+    st <- M.defaultMain theApp initialState
+    putStrLn "In input 1 you entered:\n"
+    putStrLn $ unlines $ E.getEditContents $ st^.edit1
