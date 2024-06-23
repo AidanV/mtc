f1459837224c0e7f9e05da611f9bfdc75f157d3b
AidanV <aidanvanduyne@gmail.com>
2024-06-23 10:31:11 -0700

    Added test cases

--- tests/BackendTests.hs
+++ tests/BackendTests.hs
@@ -4,11 +4,38 @@ import Calc
 import qualified System.Exit as Exit
 import Test.HUnit
 
-test1 :: Test
-test1 = TestCase (assertEqual "should return 3" 3 3)
+testAddition :: Test
+testAddition = TestCase (assertEqual "Calculating: 1 2 +" 3.0 $ calculate "1 2 +")
+
+testSubtraction :: Test
+testSubtraction = TestCase (assertEqual "Calculating: 3 2 -" 1.0 $ calculate "3 2 -")
+
+testMultiplication :: Test
+testMultiplication = TestCase (assertEqual "Calculating: 4 5 *" 20.0 $ calculate "4 5 *")
+
+testDivision :: Test
+testDivision = TestCase (assertEqual "Calculating: 6 3 /" 2.0 $ calculate "6 3 /")
+
+testExponentiation :: Test
+testExponentiation = TestCase (assertEqual "Calculating: 2 4 ^" 16.0 $ calculate "2 4 ^")
+
+testNaturalLog :: Test
+testNaturalLog = TestCase (assertEqual "Calculating: 2.718281828459045 ln" 1.0 $ calculate "2.718281828459045 ln")
+
+testSqrt :: Test
+testSqrt = TestCase (assertEqual "Calculating: 16 sqrt" 4.0 $ calculate "16 sqrt")
 
 tests :: Test
-tests = TestList [TestLabel "test1" test1]
+tests =
+  TestList
+    [ TestLabel "Test Addition" testAddition,
+      TestLabel "Test Subtraction" testSubtraction,
+      TestLabel "Test Multiplication" testMultiplication,
+      TestLabel "Test Division" testDivision,
+      TestLabel "Test Exponentiation" testExponentiation,
+      TestLabel "Test Natual Log" testNaturalLog,
+      TestLabel "Test Sqrt" testSqrt
+    ]
 
 main :: IO ()
 main = do
