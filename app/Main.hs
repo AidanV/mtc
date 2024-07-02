d7463b06b3030ac779582d417ec2da51b27dd397
AidanV <aidanvanduyne@gmail.com>
2024-07-02 14:28:48 -0700

    Dialog popping up when invalid input

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import qualified Brick.AttrMap as A
 import qualified Brick.Focus as F
 import qualified Brick.Main as M
 import qualified Brick.Types as T
+import qualified Brick.Widgets.Dialog as D
 import Brick.Util (on)
 import Brick.Widgets.Border
 import qualified Brick.Widgets.Center as C
@@ -26,18 +27,21 @@ import Lens.Micro
 import Lens.Micro.Mtl
 import Lens.Micro.TH
 
-data Name = EditEquation deriving (Ord, Show, Eq)
+data Name = EditEquation | Dialog deriving (Ord, Show, Eq)
+data DiaChoice = Enter deriving (Ord, Show, Eq)
+data DiaName = Dia deriving Show
 
 data St = St
   { _focusRing :: F.FocusRing Name,
     _previousAnswers :: [String],
-    _editEquation :: E.Editor String Name
+    _editEquation :: E.Editor String Name,
+    _maybeError :: Maybe (T.Widget Name)
   }
 
 makeLenses ''St
 
 drawUI :: St -> [T.Widget Name]
-drawUI st = [ui]
+drawUI st = maybe [ui] (:[]) (st ^. maybeError)
   where
     e1 = F.withFocusRing (st ^. focusRing) (E.renderEditor (str . unlines)) (st ^. editEquation)
     merge :: [a] -> [a] -> [a]
@@ -70,12 +74,22 @@ appEvent (T.VtyEvent (V.EvKey V.KEnter [])) = do
   s <- T.get
   let ans = calculateWithVar (s ^. previousAnswers) $ unlines $ E.getEditContents $ s ^. editEquation
   let shownAns = maybe "Not a valid equation" show ans
-
-  T.put $
-    St
-      (F.focusRing [EditEquation])
-      (take 10 (shownAns : (s ^. previousAnswers)))
-      (E.editor EditEquation (Just 1) shownAns)
+  case ans of
+    Just a ->
+      T.put $
+        St
+          (F.focusRing [EditEquation])
+          (take 10 (show a : (s ^. previousAnswers)))
+          (E.editor EditEquation (Just 1) (show a))
+          Nothing
+    Nothing ->
+      let dialog = D.dialog (Just $ str "Title") (Just (Dialog, [("Enter", Dialog, Enter)])) 50 in
+      T.put $
+        St
+          (F.focusRing [EditEquation])
+          (s ^. previousAnswers)
+          (E.editor EditEquation (Just 1) "")
+          (Just (D.renderDialog dialog $ C.hCenter $ Brick.Widgets.Core.padAll 1 $ str "This is the dialog body."))
 appEvent ev = do
   r <- use focusRing
   case F.focusGetCurrent r of
@@ -88,6 +102,7 @@ initialState =
     (F.focusRing [EditEquation])
     []
     (E.editor EditEquation (Just 1) "")
+    Nothing
 
 theMap :: A.AttrMap
 theMap =
